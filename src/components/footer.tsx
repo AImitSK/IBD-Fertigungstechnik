63e65604457bd88895c2e34f9acf4217dcc18f89
Stefan Kuehne <s.kuehne@sk-online-marketing.de>
2024-11-23 11:23:51 +0100

    Update gradients and logos in components
    Replaced gradient colors in `gradient.tsx` for a refreshed design. Substituted `Logo` with Next.js `Image` component in `footer.tsx` and `navbar.tsx` to improve image handling. Added Git mapping for the project directory in `.idea/vcs.xml`.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { Button } from './button'
 import { Container } from './container'
 import { Gradient } from './gradient'
 import { Link } from './link'
-import { Logo } from './logo'
+import Image from 'next/image'
 import { Subheading } from './text'
 
 function CallToAction() {
@@ -165,7 +165,13 @@ export function Footer() {
               <div className="grid grid-cols-2 gap-y-10 pb-6 lg:grid-cols-6 lg:gap-8">
                 <div className="col-span-2 flex">
                   <PlusGridItem className="pt-6 lg:pb-6">
-                    <Logo className="h-9" />
+                    <Image
+                      src="logo-ibd-ft.svg"
+                      alt="Logo"
+                      className="h-9"
+                      width={202}
+                      height={54}
+                    />
                   </PlusGridItem>
                 </div>
                 <div className="col-span-2 grid grid-cols-2 gap-x-8 gap-y-12 lg:col-span-4 lg:grid-cols-subgrid lg:pt-6">
